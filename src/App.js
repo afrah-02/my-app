5e0276bd2589d93b32f64bd45f2155ede97c88c7
afrah-02 <afrahfurozan2@gmail.com>
2025-02-24 12:25:12 +0530

    add  all files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
+import Register from "./Pages/Register";
+import Login from "./Pages/Login";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        // Check if user is logged in
+        const user = localStorage.getItem("username");
+        setIsLoggedIn(!!user); // Convert to boolean
+    }, []);
+
+    return (
+        <Router>
+            <Navbar isLoggedIn={isLoggedIn} />
+            <Routes>
+                {/* Public routes */}
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+
+                {/* Private routes */}
+                <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
